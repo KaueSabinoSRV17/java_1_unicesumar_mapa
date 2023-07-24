7b0a7946eef8cec6192878c4ea7ff36c8679de62
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2023-07-24 20:40:41 -0300

    refactor: patterns in tests

--- app/src/test/java/mapa/exams/CholesterolExamTest.java
+++ app/src/test/java/mapa/exams/CholesterolExamTest.java
@@ -24,40 +24,55 @@ public class CholesterolExamTest {
     @Test
     @DisplayName("Should be Good HDL if HDL by md/dl is above 45 and pacient is older than 19 years old")
     void should_be_good_hdl_if_hdl_by_md_dl_is_above_45_and_patient_is_older_than_19_years_old() {
-        CholesterolExam exam = getValidExamFromCholesterolSpecifics(getGivenYearsOldDate(19), 41, 46,
-                CholesterolRisk.LOW);
+        Date age = getGivenYearsOldDate(19);
+        int validHdl = 46;
+        int validLdl = 41;
+
+        CholesterolExam exam = getValidExamFromCholesterolSpecifics(age, validLdl, validHdl, CholesterolRisk.LOW);
         assertTrue(exam.classifyResult() == "Good Hdl Level" || exam.classifyResult() == "Both Good");
     }
 
     @Test
     @DisplayName("Should be Good HDL if HDL by md/dl is above 40 and pacient is older than 20 years old")
     void should_be_good_hdl_if_hdl_by_md_dl_is_above_40_and_patient_is_older_than_20_years_old() {
-        CholesterolExam exam = getValidExamFromCholesterolSpecifics(getGivenYearsOldDate(21), 47, 41,
-                CholesterolRisk.LOW);
+        Date age = getGivenYearsOldDate(21);
+        int validHdl = 41;
+        int validLdl = 41;
+
+        CholesterolExam exam = getValidExamFromCholesterolSpecifics(age, validLdl, validHdl, CholesterolRisk.LOW);
         assertTrue(exam.classifyResult() == "Good Hdl Level" || exam.classifyResult() == "Both Good");
     }
 
     @Test
     @DisplayName("Should be Good LDL if LDL by md/dl is below 100 and patient is of low risck")
     void should_be_good_ldl_if_ldl_by_md_dl_is_below_100_and_patient_is_of_low_risck() {
-        CholesterolExam exam = getValidExamFromCholesterolSpecifics(getGivenYearsOldDate(21), 47, 99,
-                CholesterolRisk.LOW);
+        Date age = getGivenYearsOldDate(21);
+        int validLdl = 47;
+        int validHdl = 99;
+
+        CholesterolExam exam = getValidExamFromCholesterolSpecifics(age, validLdl, validHdl, CholesterolRisk.LOW);
         assertTrue(exam.classifyResult() == "Good Ldl Level" || exam.classifyResult() == "Both Good");
     }
 
     @Test
     @DisplayName("Should be Good LDL if LDL by md/dl is below 70 and patient is of medium risck")
     void should_be_good_ldl_if_ldl_by_md_dl_is_below_70_and_patient_is_of_medium_risck() {
-        CholesterolExam exam = getValidExamFromCholesterolSpecifics(getGivenYearsOldDate(21), 47, 69,
-                CholesterolRisk.MEDIUM);
+        Date age = getGivenYearsOldDate(21);
+        int validHdl = 47;
+        int validLdl = 69;
+
+        CholesterolExam exam = getValidExamFromCholesterolSpecifics(age, validLdl, validHdl, CholesterolRisk.MEDIUM);
         assertTrue(exam.classifyResult() == "Good Ldl Level" || exam.classifyResult() == "Both Good");
     }
 
     @Test
     @DisplayName("Should be Good LDL if LDL by md/dl is below 70 and patient is of medium risck")
     void should_be_good_ldl_if_ldl_by_md_dl_is_below_50_and_patient_is_of_high_risck() {
-        CholesterolExam exam = getValidExamFromCholesterolSpecifics(getGivenYearsOldDate(21), 47, 49,
-                CholesterolRisk.LOW);
+        Date age = getGivenYearsOldDate(21);
+        int validHdl = 47;
+        int validLdl = 49;
+
+        CholesterolExam exam = getValidExamFromCholesterolSpecifics(age, validLdl, validHdl, CholesterolRisk.LOW);
         assertTrue(exam.classifyResult() == "Good Ldl Level" || exam.classifyResult() == "Both Good");
     }
 
